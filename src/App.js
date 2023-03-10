ec39c610761b3a345eb078940e6a2e570b38d79a
sharmasac <sharmasachin2698@gmail.com>
2023-03-10 12:17:22 +0530

    test this app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React,{useState} from 'react';
+import './App.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import parse from 'html-react-parser';
+
+function App() {
+
+  const [text,setText]=useState('');
+  return (
+    <div className="App">
+    <div className='editor'>
+      <CKEditor
+      
+      editor={ClassicEditor}
+      data={text}
+      onChange={(event,editor)=>{
+        const data = editor.getData();
+        setText(data)
+      }}
+      
+      />
+    </div>
+
+    <div>
+      <h2>Content Appears Here....</h2>
+      <p>{parse(text)}</p>
+    </div>
+    </div>
+
+
+  );
+}
+
+export default App;
